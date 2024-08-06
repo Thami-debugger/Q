f54eb38f3f100033908903cfb56e3e76235a5dcf
Thami-debugger <thamithami816@gmail.com>
2024-08-06 18:08:34 +0000

    Bind Flask app to 0.0.0.0 and use environment variable for port

--- main.py
+++ main.py
@@ -90,4 +90,4 @@ def logout():
     return redirect(url_for('admin_login'))
 
 if __name__ == '__main__':
-    app.run(debug=os.getenv('FLASK_DEBUG', 'False').lower() in ['true', '1'])
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=os.getenv('FLASK_DEBUG', 'False').lower() in ['true', '1'])
